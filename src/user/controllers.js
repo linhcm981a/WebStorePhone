6a0d205db157f40db09a5f36b417bb7e3dda28c5
linhcm981a <linhcm981a@gmail.com>
2023-02-22 15:01:45 +0700

    create login use jwt

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -1,42 +1,60 @@
-import User from '../user/models.js'
-import hashPassword from '../services/bcrypt.js';
-import {isValidEmail,isValidUsername } from '../services/validation.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import User from "../user/models.js";
+import hashPassword from "../services/bcrypt.js";
+import { isValidEmail, isValidUsername } from "../services/validation.js";
 
-const createNewUser = async (req, res) => {
-    try {
-      const { username, password, email } = req.body;
-      if (!username || !password) {
-        return res.status(400).json({ message: 'Username and password are required' });
-      }
-  
-      if (!isValidEmail(email)) {
-        return res.status(400).json({ message: 'Invalid email address' });
-      }
-  
-      if (!isValidUsername(username)) {
-        return res.status(400).json({ message: 'Invalid username' });
-      }
-  
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-        return res.status(400).json({ message: 'User already exists' });
-      }
-      
-      const hashedPassword = await hashPassword(password);
-  
-      const user = new User({
-        username,
-        password: hashedPassword,
-        email,
-      });
-      
-      await user.save();
-      res.status(201).json({ message: 'User created successfully', user });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Failed to create user' });
+export const createNewUser = async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password are required" });
     }
-  };
-  
 
-export default createNewUser;
+    if (!isValidEmail(email)) {
+      return res.status(400).json({ message: "Invalid email address" });
+    }
+
+    if (!isValidUsername(username)) {
+      return res.status(400).json({ message: "Invalid username" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({
+      username,
+      password: hashedPassword,
+      email,
+    });
+
+    await user.save();
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to create user" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+    
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    
+    const token = jwt.sign({ userId: user._id, email: user.email }, "my_secret_key");
+    res.json({ message: "Login successful",email, token });
+  };
